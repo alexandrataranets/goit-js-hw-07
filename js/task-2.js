be79bebb0eef620d5557887a7621f4c99960d1a5
alexandrataranets <142220751+alexandrataranets@users.noreply.github.com>
2024-01-14 01:01:08 +0100

    update css

--- js/task-2.js
+++ js/task-2.js
@@ -1,16 +1,46 @@
-const galleryList = document.querySelector(".gallery");
+// const galleryList = document.querySelector(".gallery");
+// const images = [
+//   // ... (as provided in the task)
+// ];
+
+// const createGalleryItem = (image) => {
+//   const listItem = document.createElement("li");
+//   const imageElement = document.createElement("img");
+//   imageElement.src = image.url;
+//   imageElement.alt = image.alt;
+//   listItem.appendChild(imageElement);
+//   return listItem;
+// };
+
+// const galleryItems = images.map(createGalleryItem);
+// galleryList.append(...galleryItems);
+
 const images = [
-  // ... (as provided in the task)
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    alt: 'Elephant Beside on Baby Elephant',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    alt: 'Blue Geeen and Orange Parrot',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    alt: 'Zebras on Zebra',
+  },
 ];
-
-const createGalleryItem = (image) => {
-  const listItem = document.createElement("li");
-  const imageElement = document.createElement("img");
-  imageElement.src = image.url;
-  imageElement.alt = image.alt;
-  listItem.appendChild(imageElement);
-  return listItem;
-};
-
-const galleryItems = images.map(createGalleryItem);
-galleryList.append(...galleryItems);
